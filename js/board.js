3e462815e7fca692eb7a45deb5ca07a0d1d195db
Russell <russell.tobin.miller@gmail.com>
2013-06-10 12:48:15 -0700

    factored Square code out into an object

--- js/board.js
+++ js/board.js
@@ -1,34 +1,48 @@
+var Square = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this._element = undefined;
+    this.element = function() {
+	if (this._element === undefined) {
+	    this._element = $("<div />").css({
+		top: this.x * 50 + "px",
+		left: this.y * 50 + "px"
+	    });
+	    this._element.addClass("square");
+	    this._element.addClass(squareColor(this.x, this.y));
+	    $("#board").append(this._element);
+	}
+	return this._element;
+    };
+    this.element();
+}
+
 var Board = function() {
+    this.squares = [];
     for (var i = 0; i < 8; i++) {
 	for (var j = 0; j < 8; j++) {
-	    var square = $("<div />").css({
-		top: i*50 + "px",
-		left: j*50 + "px"
-	    });
-	    square.attr("row", i);
-	    square.attr("col", j);
-	    square.addClass("square");
-	    var color = isSquareBlack(i, j) ? "black" : "white";
-	    square.addClass(color);
-	    $("#board").append(square);
+	    this.squares.push(new Square(i, j));
 	}
     }
-    var topLeft = getSquare(0, 0);
+    this.getSquare = function(i, j) {
+	for (var k = 0; k < this.squares.length; k++) {
+	    var x = this.squares[k].x;
+	    var y = this.squares[k].y;
+	    if (x == i && y == j) {
+		return this.squares[k].element();
+	    }
+	}
+    };
+    var topLeft = this.getSquare(0, 0);
     topLeft.css({ "border-top-left-radius": "10px" });
-    var bottomRight = getSquare(7, 7);
+    var bottomRight = this.getSquare(7, 7);
     bottomRight.css({ "border-bottom-right-radius": "10px" });
 }
 
-function isSquareBlack(i, j) {
-    return (i % 2) ? !(j % 2) : (j % 2);
-}
-
-function getSquare(i, j) {
-    var squares = $(".square");
-    for (var k = 0; k < squares.length; k++) {
-	var s = $(squares[k]);
-	if (s.attr("row") == i && s.attr("col") == j) {
-	    return s;
-	}
+function squareColor(i, j) {
+    if (i % 2) {
+	return !(j % 2) ? "black" : "white";
+    } else {
+	return (j % 2) ? "black" : "white";
     }
 }
