657a86c50446576d716424b075894d3f996e0044
Russell <miller.time.baby@gmail.com>
2015-02-14 11:26:09 -0800

    debug possibleSquares with a highlight toggle

--- js/chess.js
+++ js/chess.js
@@ -2,6 +2,15 @@
 
     $(document).ready(function() {
         ChessJS.board = new ChessJS.Board();
+
+        $('.piece').click(function() {
+            var piece = $(this).data('piece');
+            $.each(piece.possibleSquares(), function(idx, square) {
+                if (square.element) {
+                    square.element.toggleClass('highlight');
+                }
+            });
+        });
     });
 
 })();
